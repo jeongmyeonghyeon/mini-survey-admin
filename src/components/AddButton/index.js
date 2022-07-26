7106887fc9000bca28fb6e9281a79a11cd47502d
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-07-26 21:25:23 +0900

    redux 적용, store/reducers/action 생성, useState 교체

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -1,12 +1,65 @@
 import { PlusCircleOutlined } from '@ant-design/icons';
+import { Button, Popover, Space } from 'antd';
+import { useState } from 'react';
 import styled from 'styled-components';
 
-function AddButton({ onClick }) {
+function AddButton({ addQuestion }) {
+  const [visible, setVisible] = useState(false);
+  const hide = () => {
+    setVisible(false);
+  };
+  const handleVisibleChange = (visible) => {
+    setVisible(visible);
+  };
   return (
     <AddButtonWrapper>
-      <IconButton onClick={onClick}>
+      {/* <IconButton onClick={onClick}>
         <PlusCircleOutlined />
-      </IconButton>
+      </IconButton> */}
+      <Popover
+        content={
+          <div>
+            <Button
+              type="text"
+              onClick={() => {
+                hide();
+                addQuestion('select');
+              }}
+              style={{ display: 'block' }}
+            >
+              객관식
+            </Button>
+            <Button
+              type="text"
+              onClick={() => {
+                hide();
+                addQuestion('text');
+              }}
+              style={{ display: 'block' }}
+            >
+              단답식
+            </Button>
+            <Button
+              type="text"
+              onClick={() => {
+                hide();
+                addQuestion('textarea');
+              }}
+              style={{ display: 'block' }}
+            >
+              서술식
+            </Button>
+          </div>
+        }
+        placement="right"
+        trigger="click"
+        visible={visible}
+        onVisibleChange={handleVisibleChange}
+      >
+        <IconButton>
+          <PlusCircleOutlined />
+        </IconButton>
+      </Popover>
     </AddButtonWrapper>
   );
 }
