a2b642f3932cecbdbbeab17dfe78136bdc0fab43
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-07-26 15:06:55 +0900

    useSWR 추가, 리스트페이지 구현

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,7 +1,85 @@
+import { Table } from 'antd';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useSWR from 'swr';
+
 import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
+
+const PAGE_SIZE = 20;
+
+const columns = [
+  {
+    title: '번호',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: '제목',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: '생성일',
+    dataIndex: 'createdAt',
+    key: 'createdAt',
+    render: (createdAt) => {
+      const time = new Date(createdAt);
+
+      return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+    },
+  },
+  {
+    title: '액션',
+    dataIndex: 'id',
+    key: 'action',
+    render: (id) => {
+      return (
+        <button
+          onClick={() => {
+            console.log('삭제');
+          }}
+        >
+          삭제
+        </button>
+      );
+    },
+  },
+];
 
 function ListPage() {
-  return <MainLayout selectedKeys={'list'}>ListPage</MainLayout>;
+  const { data, error } = useSWR('/surveys', fetcher);
+  const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+
+  if (error) {
+    return 'error';
+  }
+
+  console.log(data);
+
+  return (
+    <MainLayout selectedKeys={'list'}>
+      <Table
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => {
+              console.log('onClick');
+              console.log(record.id);
+              console.log(rowIndex);
+              navigate(`/builder/${record.id}`);
+            },
+          };
+        }}
+        pagination={{ total: data.length, current: page, pageSize: PAGE_SIZE }}
+        onChange={(pagination) => {
+          setPage(pagination.current);
+        }}
+        columns={columns}
+        dataSource={data?.map((item) => ({ ...item, key: item.id }))}
+      />
+    </MainLayout>
+  );
 }
 
 export default ListPage;
